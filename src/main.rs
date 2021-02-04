693cd3456106cf6941bbfb4d4b9c3f76a219025e
R4chel <rae84@cornell.edu>
2021-02-04 17:49:12 -0500

    starting

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,22 @@
 
 #[macro_use] extern crate rocket;
 
+struct Position {
+    x : u32,
+    y : u32
+
+}
+struct Point {
+    position : Position
+}
+
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
+
+
+
 
 fn main() {
     rocket::ignite().mount("/", routes![index]).launch();
