b04ec6b757cf4e9c8d67936f66d52305a72d6de3
R4chel <rae84@cornell.edu>
2021-02-04 19:47:56 -0500

    adding more circles

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,39 @@
-use draw::{Canvas, Color, Drawing, Shape, Style, SvgRenderer};
-use rand::prelude::*;
+use rand;
+use rgb::RGB;
+use draw::{Canvas, Drawing, Shape, Style, SvgRenderer};
 
+fn update_pos(min_pos:f32, max_pos:f32, current:f32) ->  f32 {
+    
+    (current + if rand::random() { 1.0 } else { -1.0 }).max(min_pos).min(max_pos)
+}
+
+fn update_color(current : u8) ->  u8{
+    if rand::random() { current + 10 } else { current - 10}
+}
 fn main() {
-    let mut canvas = Canvas::new(100, 100);
+    let length : u8 = 100;
+    let mut canvas = Canvas::new(u32::from(length), u32::from(length));
 
-    let how_many: u8 = 3;
+    let how_many = 10;
     let mut x_pos = 10.0;
     let mut y_pos = 10.0;
+    let mut r = 128;
+    let mut g = 128;
+    let mut b = 128;
 
-    let mut rng = thread_rng();
+    let update_pos = move |pos| update_pos(0.0,f32::from(length), pos);  
 
     for _ in 0..how_many {
-        x_pos = &x_pos + if rng.gen() { 1.0 } else { -1.0 };
-
-        y_pos = &y_pos + if rng.gen() { 1.0 } else { -1.0 };
-
+        x_pos = update_pos(x_pos);
+        y_pos = update_pos(y_pos);
+        r = update_color(r);
+        g = update_color(g);
+        b = update_color(b);
+        
         let circle = Drawing::new()
             .with_shape(Shape::Circle { radius: 1 })
             .with_xy(x_pos, y_pos)
-            .with_style(Style::filled(Color::random()));
+            .with_style(Style::filled(RGB::new(r,g,b)));
 
         canvas.display_list.add(circle);
     }
