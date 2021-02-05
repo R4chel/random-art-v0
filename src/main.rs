7b40844bbc5368a278417448052af1f2f7f96a40
R4chel <rae84@cornell.edu>
2021-02-04 20:02:57 -0500

    formatting

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,40 @@
+use draw::{Canvas, Drawing, Shape, Style, SvgRenderer};
 use rand;
 use rgb::RGB;
-use draw::{Canvas, Drawing, Shape, Style, SvgRenderer};
 
-fn update_pos(min_pos:f32, max_pos:f32, current:f32) ->  f32 {
-    
-    (current + if rand::random() { 1.0 } else { -1.0 }).max(min_pos).min(max_pos)
+fn update_pos(min_pos: f32, max_pos: f32, current: f32) -> f32 {
+    let offset = 1.0;
+    (current
+        + if rand::random() {
+            1.0 * offset
+        } else {
+            -1.0 * offset
+        })
+    .max(min_pos)
+    .min(max_pos)
 }
 
-fn update_color(current : u8) ->  u8{
+fn update_color(current: u8) -> u8 {
     let offset = 10;
-    (
-        if rand::random() { current.checked_add(offset) } else { current.checked_sub(offset)}
-    ).unwrap_or(current)
+    (if rand::random() {
+        current.checked_add(offset)
+    } else {
+        current.checked_sub(offset)
+    })
+    .unwrap_or(current)
 }
 fn main() {
-    let length : u16 = 100;
+    let length: u16 = 100;
     let mut canvas = Canvas::new(u32::from(length), u32::from(length));
 
-    let how_many = 10000;
+    let how_many = 20000;
     let mut x_pos = 10.0;
     let mut y_pos = 10.0;
     let mut r = 128;
     let mut g = 128;
     let mut b = 128;
 
-    let update_pos = move |pos| update_pos(0.0,f32::from(length), pos);  
+    let update_pos = move |pos| update_pos(0.0, f32::from(length), pos);
 
     for _ in 0..how_many {
         x_pos = update_pos(x_pos);
@@ -32,11 +42,11 @@ fn main() {
         r = update_color(r);
         g = update_color(g);
         b = update_color(b);
-        
+
         let circle = Drawing::new()
             .with_shape(Shape::Circle { radius: 1 })
             .with_xy(x_pos, y_pos)
-            .with_style(Style::filled(RGB::new(r,g,b)));
+            .with_style(Style::filled(RGB::new(r, g, b)));
 
         canvas.display_list.add(circle);
     }
